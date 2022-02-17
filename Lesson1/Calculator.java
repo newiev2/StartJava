45f66f558e63f7e1e3a8c1e7d71bc302d288e8ef
Nikita B <qopkawrka@gmail.com>
2022-02-17 16:26:42 +0300

    Final homework lesson 1. Added Calculator, GuessNumber, UnicodeCycle

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,50 @@
+public class Calculator {
+    public static void main(String[] args) {
+        //Программа-калькулятор. Выполняет операцию, хранящуюся в operator, над целочисленными положительными операндами operand1 и operand2
+        System.out.println("Программа-калькулятор. Выполняет операцию, хранящуюся в operator, над целочисленными положительными операндами operand1 и operand2");
+
+        int operand1 = 50;
+        int operand2 = 1;
+        char operator = '/';
+        double operationResult = 0;
+        boolean isExists = true;
+
+        System.out.println("Operand1 = " + operand1 + "\n \r" + "Operand2 = " + operand2 + "\n \r" + "Operator = " + operator);
+
+        if(operator == '+') {
+            operationResult = operand1 + operand2;
+        } else if(operator == '-') {
+            operationResult = operand1 - operand2;
+        } else if(operator == '*') {
+            operationResult = operand1 * operand2;
+        } else if(operator == '/') {
+            if(operand2 == 0) {
+                isExists = false;
+            } else {
+                operationResult = operand1 / operand2;
+            }
+        } else if(operator == '^') {
+            if(operand2 == 0) {
+                operationResult = 1;
+            } else if(operand2 == 1) {
+                operationResult = operand1;
+            } else {
+                operationResult = operand1;
+                for(int i = 2; i <= operand2; i++) {
+                    operationResult = operationResult * operand1;
+                }
+            }
+        } else if(operator == '%') {
+            operationResult = operand1 % operand2;
+        } else {
+            System.out.println("Такая операция в программе не реализована либо значение оператора не является оператором");
+        }
+
+        if(isExists) {
+            System.out.println("Результат операции " + operand1 + "" + operator + "" + operand2 + " равен " + operationResult);
+        } else {
+            System.out.println("На ноль делить нельзя! Результата операции " + operand1 + "" + operator + "" + operand2 + " не существует");
+        }
+        
+    }
+}
---
+++ Lesson1/UnicodeCycle.java
@@ -0,0 +1,13 @@
+public class UnicodeCycle {
+    public static void main(String[] args) {
+        //Данная программа выводит символы Юникода в определенном диапазоне [33, 126]
+        System.out.println("Данная программа выводит символы Юникода в определенном диапазоне [33, 126]");
+
+        char initSign = 33;
+
+        do {
+            System.out.println((int) initSign + " " + initSign);
+            initSign++;
+        } while(initSign <= 126);
+    }
+}
