ca88dc9481e47c5db1ebbd075392ca3c9ab6737a
Nikita B <qopkawrka@gmail.com>
2022-02-17 18:43:22 +0300

    Calculator and GuessNumber fixes

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -3,47 +3,45 @@ public class Calculator {
         //Программа-калькулятор. Выполняет операцию, хранящуюся в operator, над целочисленными положительными операндами operand1 и operand2
         System.out.println("Программа-калькулятор. Выполняет операцию, хранящуюся в operator, над целочисленными положительными операндами operand1 и operand2");
 
-        int operand1 = 50;
-        int operand2 = 1;
-        char operator = '(';
-        double operationResult = 0;
+        int operand1 = 4;
+        int operand2 = 4;
+        char operator = '^';
+        double result = 0;
         boolean isExists = true;
 
-        System.out.println("Operand1 = " + operand1 + "\n \r" + "Operand2 = " + operand2 + "\n \r" + "Operator = " + operator);
-
         if(operator == '+') {
-            operationResult = operand1 + operand2;
+            result = operand1 + operand2;
         } else if(operator == '-') {
-            operationResult = operand1 - operand2;
+            result = operand1 - operand2;
         } else if(operator == '*') {
-            operationResult = operand1 * operand2;
+            result = operand1 * operand2;
         } else if(operator == '/') {
             if(operand2 == 0) {
-                isExists = false;
+                System.out.println("Деление на 0 запрещено");
             } else {
-                operationResult = operand1 / operand2;
+                result = operand1 / operand2;
             }
         } else if(operator == '^') {
             if(operand2 == 0) {
-                operationResult = 1;
+                result = 1;
             } else if(operand2 == 1) {
-                operationResult = operand1;
+                result = operand1;
             } else {
-                operationResult = operand1;
+                result = operand1;
                 for(int i = 2; i <= operand2; i++) {
-                    operationResult = operationResult * operand1;
+                    result *= operand1;
                 }
             }
         } else if(operator == '%') {
-            operationResult = operand1 % operand2;
+            result = operand1 % operand2;
         } else {
             isExists = false;
         }
 
         if(isExists) {
-            System.out.println("Результат операции " + operand1 + "" + operator + "" + operand2 + " равен " + operationResult);
+            System.out.println("Результат операции " + operand1 + "" + operator + "" + operand2 + " равен " + result);
         } else {
-            System.out.println("Операция " + operand1 + "" + operator + "" + operand2 + " не существует либо вы пытаетесь делить на ноль");
+            System.out.println("Вы ввели недопустимую операцию " + operator);
         }
     }
 }
--- Lesson1/GuessNumber.java
+++ Lesson1/GuessNumber.java
@@ -3,7 +3,7 @@ public class GuessNumber {
         //Игра "Угадай число!". Введите любое число в диапазоне от 0 до 100 с клавиатуры. Если ввёденное число будет равнятся запрограммированному компьютером числу, вы выиграли
         System.out.println("Игра 'Угадай число!'. Если угаданное число будет равнятся запрограммированному компьютером числу, вы выиграли");
 
-        byte computedNum = 10;
+        byte computedNum = 17;
         byte tryNum = 5;
 
         System.out.println("Загаданное число: " + tryNum);
@@ -11,14 +11,13 @@ public class GuessNumber {
         do {
             if((tryNum > 100) || (computedNum > 100)) {
                 System.out.println("К сожалению, одно из чисел вышло за пределы диапазона. Попытайтесь угадать снова");
-                computedNum = 10;
+                computedNum = 17;
                 tryNum = 5;
             } else if(tryNum > computedNum) {
                 System.out.println("Данное число больше того, что загадал компьютер");
             } else {
                 System.out.println("Данное число меньше того, что загадал компьютер");
             }
-            computedNum += 2;
             tryNum += 3;
             System.out.println("Вы загадали новое число: " + tryNum);
         } while(tryNum != computedNum);
