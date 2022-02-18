52798f7c4b8b44b6290473710f85efd0f04de291
Nikita B <qopkawrka@gmail.com>
2022-02-18 17:28:58 +0300

    Calculator GuessNumber final fixes

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,14 +1,11 @@
-public class Calculator {
+public class Calculator1 {
     public static void main(String[] args) {
         //Программа-калькулятор. Выполняет операцию, хранящуюся в operator, над целочисленными положительными операндами operand1 и operand2
         System.out.println("Программа-калькулятор. Выполняет операцию, хранящуюся в operator, над целочисленными положительными операндами operand1 и operand2");
-
-        int operand1 = 4;
-        int operand2 = 4;
+        int operand1 = 5;
+        int operand2 = 1;
         char operator = '^';
         double result = 0;
-        boolean isExists = true;
-
         if(operator == '+') {
             result = operand1 + operand2;
         } else if(operator == '-') {
@@ -22,26 +19,15 @@ public class Calculator {
                 result = operand1 / operand2;
             }
         } else if(operator == '^') {
-            if(operand2 == 0) {
-                result = 1;
-            } else if(operand2 == 1) {
-                result = operand1;
-            } else {
-                result = operand1;
-                for(int i = 2; i <= operand2; i++) {
-                    result *= operand1;
-                }
+            result = 1;
+            for(int i = 0; i < operand2; i++) {
+                result *= operand1;
             }
         } else if(operator == '%') {
             result = operand1 % operand2;
         } else {
-            isExists = false;
+            System.out.println("Вы ввели недопустимый знак " + operator);
+            return;
         }
-
-        if(isExists) {
-            System.out.println("Результат операции " + operand1 + "" + operator + "" + operand2 + " равен " + result);
-        } else {
-            System.out.println("Вы ввели недопустимую операцию " + operator);
-        }
+        System.out.println("Результат операции " + operand1 + "" + operator + "" + operand2 + " равен " + result);
     }
-}
--- Lesson1/GuessNumber.java
+++ Lesson1/GuessNumber.java
@@ -3,25 +3,23 @@ public class GuessNumber {
         //Игра "Угадай число!". Введите любое число в диапазоне от 0 до 100 с клавиатуры. Если ввёденное число будет равнятся запрограммированному компьютером числу, вы выиграли
         System.out.println("Игра 'Угадай число!'. Если угаданное число будет равнятся запрограммированному компьютером числу, вы выиграли");
 
-        byte computedNum = 17;
-        byte tryNum = 5;
-
+        byte computedNum = 13;
+        byte tryNum = 102;
         System.out.println("Загаданное число: " + tryNum);
 
         do {
-            if((tryNum > 100) || (computedNum > 100)) {
-                System.out.println("К сожалению, одно из чисел вышло за пределы диапазона. Попытайтесь угадать снова");
-                computedNum = 17;
-                tryNum = 5;
+            if((tryNum > 100) || (tryNum < 0)) {
+                System.out.println("Вы загадали число, не входящее в диапазон от 0 до 100");
+                tryNum = 0;
             } else if(tryNum > computedNum) {
                 System.out.println("Данное число больше того, что загадал компьютер");
+                tryNum -= 1;
             } else {
                 System.out.println("Данное число меньше того, что загадал компьютер");
+                tryNum += 1;
             }
-            tryNum += 3;
             System.out.println("Вы загадали новое число: " + tryNum);
         } while(tryNum != computedNum);
-
         //Число угадано. Выход из цикла с сообщением о победе в игре
         System.out.println("Поздравляю, число угадано!");
     }
