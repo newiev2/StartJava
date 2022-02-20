77e73fa06f60bb20f917418de0e57c7beb5f8e6c
Nikita B <qopkawrka@gmail.com>
2022-02-20 18:33:18 +0300

    Added Person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "John";
+    String sex = "Male";
+    int age = 25;
+    float height = 1.78F;
+    float weight = 80.5F;
+
+    void move() {
+        System.out.println("You start moving");
+    }
+
+    void sit() {
+        System.out.println("You've just sat");
+    }
+
+    void run() {
+        System.out.println("You start running");
+    }
+
+    void say() {
+        System.out.println("Hello! My name is ...");
+    }
+
+    void learnJava() {
+        System.out.println("You start learning Java");
+    }
+}
